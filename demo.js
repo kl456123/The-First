575b7c086f196fa397f81ab206e6463f24774ee6
breakpoint <kl456123@outlook.com>
2016-12-12 23:08:37 +0800

    add js script writen by ES6 synax

---
+++ demo.js
@@ -0,0 +1,19 @@
+//
+//
+//The file is just for pulling requests
+
+let name = 'breakpoint';
+
+console.log(name);
+
+function sayHello(name){
+  console.log('hello '+name);
+}
+
+
+sayHello(name);
+
+
+
+
+
